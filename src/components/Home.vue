090bebafdbc75d71b4a40a09ae9c45d0db3e3a22
Arcobale <1144209486@qq.com>
2023-04-01 01:36:05 +0800

    CSS预处理器，样式重置

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <el-button type="primary">按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.hello {
+    background-color: yellow;
+    .el-button {
+        color:red;
+    }
+}
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,4 @@
+//按需引入element组件
+import Vue from 'vue'
+import { Button } from 'element-ui'
+Vue.use(Button);
