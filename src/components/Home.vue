0fd4f08b2cbcfdf702c3fdcfc83db9b80493c7e1
Arcobale <1144209486@qq.com>
2023-04-01 21:33:02 +0800

    拆分静态组件

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,41 @@
 <template>
-  <div class="hello">
-    <el-button type="primary">按钮</el-button>
-    <i class="fa fa-user"></i>
+  <div class="home">
+    <Header>Header</Header>
+    <el-container class="content">
+      <Menu></Menu>
+      <el-container>
+        <el-main>Main</el-main>
+        <el-footer>
+          <Footer></Footer>
+        </el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import Header from './common/Header.vue';
+import Footer from './common/Footer.vue';
+import Menu from './common/Menu.vue';
 export default {
-
+  components: {
+    Header,
+    Footer,
+    Menu,
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.hello {
-    background-color: yellow;
-    .el-button {
-        color:red;
-    }
+.home {
+  width: 100%;
+  height: 100%;
+  .content {
+    position: absolute;
+    width: 100%;
+    top: 60px;
+    bottom: 0;
+    background: pink;
+  }
 }
 </style>
