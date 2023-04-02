ab00c502aa42847539a5647d6ab03604a857656d
Arcobale <1144209486@qq.com>
2023-04-02 22:22:58 +0800

    echarts地图绘制

--- src/components/dataAnalysis/MapView.vue
+++ src/components/dataAnalysis/MapView.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>地图预览</div>
+  <div class="map-view">
+    <div id="main"></div>
+  </div>
 </template>
 
 <script>
+import geoJson from '@/assets/data.json';
 export default {
+  data() {
+    return {
 
+    }
+  },
+  mounted() {
+    let myChart = this.$echarts.init(document.getElementById('main'));
+    this.$echarts.registerMap('china', geoJson); //注册可用的地图
+    let option = {
+      //地理坐标系组件
+      geo: {
+        map: 'china',
+        aspectScale: 0.75, // 地图长宽比
+        zoom: 1.1,
+        regions: [{
+          name: '南海诸岛',
+        }]
+      },
+      //配置地图相关参数，绘制地图，关于地图图表的相关设置
+      series: [
+        {
+          type: 'map',
+          zoom: 1.1,
+          map: 'china',
+        }
+      ]
+    };
+    myChart.setOption(option);
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.map-view {
+  width: 100%;
+  #main {
+    width: 100%;
+    height: 600px;
+  }
+}
 </style>
