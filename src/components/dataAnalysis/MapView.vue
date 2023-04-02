6836a0ed45f93e54b54f114b4303a1f8b182afa3
Arcobale <1144209486@qq.com>
2023-04-02 22:32:35 +0800

    地图区域样式

--- src/components/dataAnalysis/MapView.vue
+++ src/components/dataAnalysis/MapView.vue
@@ -16,11 +16,34 @@ export default {
     let myChart = this.$echarts.init(document.getElementById('main'));
     this.$echarts.registerMap('china', geoJson); //注册可用的地图
     let option = {
+      backgroundColor: 'rgb(121, 145, 209)',
       //地理坐标系组件
       geo: {
         map: 'china',
         aspectScale: 0.75, // 地图长宽比
         zoom: 1.1,
+        itemStyle: {
+          normal: {
+            areaColor: {
+              type: 'radial', //径向渐变
+              x: 0.5,
+              y: 0.5,
+              r: 0.8, //半径
+              colorStops: [
+                {
+                  //0%处的颜色
+                  offset: 0,
+                  color: '#09132c',
+                },
+                {
+                  offset: 1,
+                  color: '#274d68',
+                }
+              ],
+              globalCoord: true,
+            }
+          }
+        },
         regions: [{
           name: '南海诸岛',
         }]
@@ -31,6 +54,35 @@ export default {
           type: 'map',
           zoom: 1.1,
           map: 'china',
+          itemStyle: {
+            backgroundColor: 'rgb(147, 235, 248)',
+            borderWidth: 1,
+            normal: {
+              areaColor: {
+                type: 'radial', //径向渐变
+                x: 0.5,
+                y: 0.5,
+                r: 0.8, //半径
+                colorStops: [
+                  {
+                    //0%处的颜色
+                    offset: 0,
+                    color: 'rgb(31, 54, 150)',
+                  },
+                  {
+                    offset: 1,
+                    color: 'rgb(89, 128, 142)',
+                  }
+                ],
+                globalCoord: true,
+              }
+            },
+            //设置高亮
+            emphasis: {
+              areaColor: 'rgb(46, 229, 206)',
+              borderWidth: 0.1,
+            }
+          },
         }
       ]
     };
@@ -42,6 +94,7 @@ export default {
 <style lang="scss">
 .map-view {
   width: 100%;
+
   #main {
     width: 100%;
     height: 600px;
